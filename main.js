0b3d917e0b436b9d0b8a98b0fe348d71dc6d8405
JulieC01 <124850153+JulieC01@users.noreply.github.com>
2023-03-26 22:25:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+    // make the div called "mySketch" a p5.js canvas element
+    let cnv = createCanvas(windowWidth, windowHeight);
+    cnv.parent('mySketch');
+
+  }
+  
+  function draw() {
+    
+  }
