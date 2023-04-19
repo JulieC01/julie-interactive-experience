0ca825c844e0d877f7ba2c75523cc82f55a66567
JulieC01 <124850153+JulieC01@users.noreply.github.com>
2023-04-19 14:20:02 -0700

    Delete Exerise 5 directory

--- Exerise 5/modes/mode1.js
+++
@@ -1,6 +0,0 @@
-let forrestBg1;
-function forrest1(){
-    forrestBg1 = createImg('images/forrest1.jpeg');
-    forrestBg1.position(0, 0);
-    forrestBg1.size(width, height);
-}
--- Exerise 5/modes/mode2.js
+++
@@ -1,6 +0,0 @@
-let forrestBg2;
-function forrest2(){
-    forrestBg2 = createImg('images/forrest2.jpeg');
-    forrestBg2.position(0, 0);
-    forrestBg2.size(width, height);
-}
